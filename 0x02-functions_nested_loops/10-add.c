15ff8e83a4eaba65f0425b59a485339772f3bb4a
tafesegechera <tafesegechera22@gmail.com>
2022-06-07 15:36:13 -0700

    addition of two numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,14 +1,15 @@
-include "main.h"
+ls
+#include "main.h"
 #include <stdio.h>
 
 /**
- * add - adds two integers and returns the result(sum)
+ * add - adds two integers and returns the result
  * @i: print int i
  * @k: print int k
  *
  * Description: a function that adds two integers and returns the result
  *
- * Return: always 0.
+ * Return: Always 0.
  */
 
 int add(int i, int k)
