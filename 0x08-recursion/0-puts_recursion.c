f78ccead7ad6fdd42f6ffdd38f7552cc7d7f0c7c
tafesegechera <tafesegechera22@gmail.com>
2022-06-22 23:35:46 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,9 @@
 #include "main.h"
 /**
-
  * _puts_recursion - function that prints a string, followed by a new line.
  * @s: string to recurse
  * Return: output
-*/
-
+ */
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
