91f8cbf148e7466dfcf370bb198eedf3e179ccfa
tafesegechera <tafesegechera22@gmail.com>
2022-06-23 01:19:01 -0700

    main.h

--- main.h
+++ main.h
@@ -1,18 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-int _atoi(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
 int _putchar(char c);
-#endif /* MAIN>H */
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+
+#endif
