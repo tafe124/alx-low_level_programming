9339a59fa3efbb23116468dbe7b5e3811ec05895
tafe124 <tafesegechera22@gmail.com>
2022-09-16 04:58:52 -0700

    last task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -q -O /tmp/winwin.so https://github.com/ermiasbahru/alx-low_level_programming/raw/master/0x18-dynamic_libraries/winwin.so
+wget -q -O /tmp/winwin.so https://github.com/tafe124/alx-low_level_programming/raw/master/0x18-dynamic_libraries/winwin.so
 export LD_PRELOAD=/tmp/winwin.so
+
